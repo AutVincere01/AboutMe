207a709faff0aefd7e05105ed42cb22b00cd4d0c
AutVincere01 <103220587+AutVincere01@users.noreply.github.com>
2022-04-18 01:22:39 +0200

    Portfolio

---
+++ meetover/Pierwsza/Druga/index.js
@@ -0,0 +1,11 @@
+const first=(
+    <div>
+        <p1>Well I said Continue...</p1>
+        <br />
+        <p1>Start paying attention please.</p1>
+        <br />
+        <p1>It's here btw.</p1>
+    </div>
+)
+
+ReactDOM.render(first,document.getElementById("dial"))
